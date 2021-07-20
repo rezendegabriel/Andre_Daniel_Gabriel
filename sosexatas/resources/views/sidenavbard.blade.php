99a49a6e702b07984677f722742f93633d7a95ad
rezendegabriel <rezende.gabriel@engenharia.ufjf.br>
2021-07-19 22:37:44 -0300

    barra lateral

--- sosexatas/resources/views/sidenavbard.blade.php
+++ sosexatas/resources/views/sidenavbard.blade.php
@@ -1,57 +1,14 @@
+<link rel = "stylesheet" href = "https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/css/adminlte.min.css">
+
 <aside class = "main-sidebar sidebar-dark-primary elevation-4">
     <!-- Logo -->
-    <a href = "" class = "brand-link">
+    <a href = "#" class = "brand-link">
         <span class = "brand-text font-weight-light">S.O.S. EXATAS</span>
     </a>
 
     <!-- Barra lateral -->
-    <div class = "sidebar os-host os-theme-light os-host-overflow os-host-overflow-y os-host-resize-disabled os-host-scrollbar-horizontal-hidden os-host-transition">
-        <div class = "os-resize-observer-host observed">
-            <div class = "os-resize-observer" style = "left: 0px; right: auto;"></div>
-        </div>
-        <div class = "os-size-auto-observer observed" style = "height: calc(100% + 1px); float: left;">
-            <div class = "os-resize-observer"></div>
-        </div>
-        <div class = "os-content-glue" style = "margin: 0px -8px; width: 249px; height: 911px;"></div>
-        <div class = "os-padding">
-            <div class = "os-viewport os-viewport-native-scrollbars-invisible" style = "overflow-y: scroll;">
-                <div class = "os-content" style = "padding: 0px 8px; height: 100%; width: 100%;"></div>
-            </div>
-        </div>
-        
-        <!-- Usuário login
-        <div class = "user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class = "image">
-                <img src = "" class = "img-circle elevation-2" alt = "">
-            </div>
-            <div class = "info">
-                <a href = "" class = "d-block">Nome do usuário</a>
-            </div>
-        </div>
-        -->
-        
-        <!-- Barra de pesquisa
-        <div class = "form-inline">
-            <div class = "input-group" data-widget = "sidebar-search">
-                <input class = "form-control form-control-sidebar" type = "search" placeholder = "Search" aria-label = "Search">
-                <div class = "input-group-append">
-                    <button class = "btn btn-sidebar">
-                        <i class = "fas fa-search fa-fw"></i>
-                    </button>
-                </div>
-            </div>
-            <div class = "sidebar-search-results">
-                <div class = "list-group">
-                    <a href = "" class = "list-group-item">
-                        <div class = "search-title">
-                            <strong class = "text-light"></strong>N<strong class = "text-light"></strong>o<strong class = "text-light"></strong> <strong class = "text-light"></strong>e<strong class = "text-light"></strong>l<strong class = "text-light"></strong>e<strong class = "text-light"></strong>m<strong class = "text-light"></strong>e<strong class = "text-light"></strong>n<strong class = "text-light"></strong>t<strong class = "text-light"></strong> <strong class = "text-light"></strong>f<strong class = "text-light"></strong>o<strong class = "text-light"></strong>u<strong class = "text-light"></strong>n<strong class = "text-light"></strong>d<strong class = "text-light"></strong>!<strong class = "text-light"></strong></div><div class = "search-path">
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-        -->
-
+    <div class = "sidebar">
+        <div class = "os-content-glue" style = "margin: 0px-8px; width: 249px; height: 911px;"></div>
         <!-- Menu -->
         <nav class = "mt-2">
             <ul class = "nav nav-pills nav-sidebar flex-column" data-widget = "treeview" role = "menu" data-accordion = "false">
@@ -62,19 +19,19 @@
                     </a>
                     <ul class = "nav nav-treeview">
                         <li class = "nav-item">
-                            <a href = "" class = "nav-link">
+                            <a href = "#" class = "nav-link">
                                 <i class = "far fa-circle nav-icon"></i>
                                 <p>Disciplina 1</p>
                             </a>
                         </li>
                         <li class = "nav-item">
-                            <a href = "" class = "nav-link">
+                            <a href = "#" class = "nav-link">
                                 <i class = "far fa-circle nav-icon"></i>
                                 <p>Disciplina 2</p>
                             </a>
                         </li>
                         <li class = "nav-item">
-                            <a href = "" class = "nav-link">
+                            <a href = "#" class = "nav-link">
                                 <i class = "far fa-circle nav-icon"></i>
                                 <p>Disciplina 3</p>
                             </a>
@@ -84,15 +41,6 @@
             </ul>
         </nav>
     </div>
-    <div class = "os-scrollbar os-scrollbar-horizontal os-scrollbar-unusable os-scrollbar-auto-hidden">
-        <div class = "os-scrollbar-track">
-            <div class = "os-scrollbar-handle" style = "width: 100%; transform: translate(0px, 0px);"></div>
-        </div>
-    </div>
-    <div class = "os-scrollbar os-scrollbar-vertical os-scrollbar-auto-hidden">
-        <div class = "os-scrollbar-track">
-            <div class = "os-scrollbar-handle" style = "height: 57.1071%; transform: translate(0px, 0px);"></div>
-        </div>
-    </div>
-    <div class = "os-scrollbar-corner"></div>
 </aside>
+
+<script src = "https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/js/adminlte.min.js"></script>
